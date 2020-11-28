573b3e49f2830553715cb8e7e3eaa80e55aa6c8b
Erin Black <erinblackdesign@gmail.com>
2020-11-28 11:58:07 -0600

    Added db for post and categories

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+    def index
+        @content_first = 'This is the first paragraph';
+        @content_second = 'This is the second blog post right here!';
+    end
+
+    def new
+
+    end
+    
+    def create
+    
+    end
+    
+    def edit
+
+    end
+
+    def udpate
+
+    end
+
+    def show
+
+    end
+
+    def destroy
+
+    end
+end
