6876d9ae6d6c812f5dc7682d2ed7a70da86ce82c
Erin Black <erinblackdesign@gmail.com>
2020-11-29 13:52:42 -0600

    added categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ApplicationRecord
+    include ActiveModel::ForbiddenAttributesProtection
+    def user_params
+        params.require(:name).permit(:name)
+      end
+    has_many :posts
 end
